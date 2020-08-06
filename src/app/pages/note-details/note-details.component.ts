b757c8201cf0b8c12a1b10273f0bda1661a44829
MLHocalhost <18cy039@esisalama.org>
2020-08-06 14:20:38 +0200

    feat (note-details) : create note service

--- src/app/pages/note-details/note-details.component.ts
+++ src/app/pages/note-details/note-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Note } from 'src/app/shared/note.model';
+import { NotesService } from 'src/app/shared/notes.service';
 
 @Component({
   selector: 'app-note-details',
@@ -11,14 +12,14 @@ export class NoteDetailsComponent implements OnInit {
 
   note : Note ;
 
-  constructor() { }
+  constructor(private notesService : NotesService) { }
 
   ngOnInit(): void {
     this.note = new Note();
   }
 
   onSubmit(form :NgForm){
-    
+    this.notesService.add(form.value);
   }
 
 }
