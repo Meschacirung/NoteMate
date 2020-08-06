fec289371db455b470b2bf02838376ead163a564
MLHocalhost <18cy039@esisalama.org>
2020-08-06 07:21:04 +0200

    feat : init project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { NoteListComponent } from './pages/note-list/note-list.component';
+
+
+const routes: Routes = [
+  { path :'', component: NoteListComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NoteListComponent } from './pages/note-list/note-list.component';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NoteListComponent,
+    MainLayoutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
