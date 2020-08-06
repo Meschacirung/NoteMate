eb82c1be623a856a1f595653a70c671a11efb666
MLHocalhost <18cy039@esisalama.org>
2020-08-06 13:31:34 +0200

    feat (note-details) : add form validation

--- src/app/pages/note-details/note-details.component.ts
+++ src/app/pages/note-details/note-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Note } from 'src/app/shared/note.model';
 
 @Component({
   selector: 'app-note-details',
@@ -8,13 +9,16 @@ import { NgForm } from '@angular/forms';
 })
 export class NoteDetailsComponent implements OnInit {
 
+  note : Note ;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.note = new Note();
   }
 
   onSubmit(form :NgForm){
-    console.log(form);
+    
   }
 
 }
