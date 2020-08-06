ea557b009e4388178a1c5062f41d47941156efe0
MLHocalhost <18cy039@esisalama.org>
2020-08-06 15:22:52 +0200

    feat (note-details) : create update method

--- src/app/pages/note-details/note-details.component.ts
+++ src/app/pages/note-details/note-details.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Note } from 'src/app/shared/note.model';
 import { NotesService } from 'src/app/shared/notes.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-note-details',
@@ -12,16 +12,37 @@ import { Router } from '@angular/router';
 export class NoteDetailsComponent implements OnInit {
 
   note : Note ;
+  noteId : number;
+  new : boolean;
 
-  constructor(private notesService : NotesService, private router : Router) { }
+  constructor(private notesService : NotesService, private router : Router, private route : ActivatedRoute) { }
 
   ngOnInit(): void {
+
     this.note = new Note();
+
+    this.route.params.subscribe((params : Params) =>{
+      if (params.id){
+        this.note = this.notesService.get(params.id);
+        this.noteId = params.id;
+        this.new = false;
+      } else{
+        this.new = true;
+      }
+    });
+
   }
 
   onSubmit(form :NgForm){
-    this.notesService.add(form.value);
+
+    if(this.new){
+      this.notesService.add(form.value);
+    } else {
+      this.notesService.update(this.noteId, form.value.title, form.value.body);
+    }
+
     this.router.navigateByUrl('/')
+
   }
 
   cancel(){
