750f182a4ceb943cefea93eeae817679000e4167
MLHocalhost <18cy039@esisalama.org>
2020-08-06 11:35:44 +0200

    feat (note-card) : create inputs title & body

--- src/app/note-card/note-card.component.ts
+++ src/app/note-card/note-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Renderer2, Input } from '@angular/core';
 
 @Component({
   selector: 'app-note-card',
@@ -7,21 +7,24 @@ import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/co
 })
 export class NoteCardComponent implements OnInit {
 
-  @ViewChild('bodyText') bodyText : ElementRef<HTMLElement>;
-  @ViewChild('truncator') truncator : ElementRef<HTMLElement>;
+  @Input('title') title : String;
+  @Input('body') body : String;
 
-  constructor(private renderer : Renderer2) { }
+  // @ViewChild('truncator') truncator : ElementRef<HTMLElement>;
+  // @ViewChild('bodyText') bodyText : ElementRef<HTMLElement>;
+
+  // constructor(private renderer : Renderer2) { }
 
   ngOnInit(){
 
-    let style = window.getComputedStyle(this.bodyText.nativeElement, null);
-    let viewableHeight = parseInt(style.getPropertyValue("height"), 10);
+  //   let style = window.getComputedStyle(this.bodyText.nativeElement, null);
+  //   let viewableHeight = parseInt(style.getPropertyValue("height"), 10);
 
-    if (this.bodyText.nativeElement.scrollHeight > viewableHeight){
-      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'block');
-    } else{
-      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'none');
-    }
+  //   if (this.bodyText.nativeElement.scrollHeight > viewableHeight){
+  //     this.renderer.setStyle(this.truncator.nativeElement, 'display', 'block');
+  //   } else{
+  //     this.renderer.setStyle(this.truncator.nativeElement, 'display', 'none');
+  //   }
 
   }
 
