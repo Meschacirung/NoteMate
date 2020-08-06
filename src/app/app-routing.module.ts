d9e35d7c86cc7252590a7ba0c2f5c1042531ac27
MLHocalhost <18cy039@esisalama.org>
2020-08-06 11:49:09 +0200

    feat (note-details) : create route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NoteListComponent } from './pages/note-list/note-list.component';
 import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { NoteDetailsComponent } from './pages/note-details/note-details.component';
 
 
 const routes: Routes = [
   { path :'', component: MainLayoutComponent, children :[
-    { path :'', component : NoteListComponent }
+    { path :'', component : NoteListComponent },
+    { path :':id', component : NoteDetailsComponent }
   ]}
 ];
 
